9a6ea2b319795f5e52e89e77d55e1ef732309c05
Alexandra Petrova <aepetrova00@mail.ru>
2024-11-27 11:17:12 +0700

    Fixed some bugs and added docstrings

--- class_Location.py
+++ class_Location.py
@@ -1,45 +1,63 @@
-import requests
 import json
 
+import requests
 
-class GetLocation:
-	"""
-	Класс описывает местоположение.
-	"""
-	def __init__(self, city: str):
-		self.__city = city
-		self.__url = f'https://geocoding-api.open-meteo.com/v1/search?name={self.__city}&count=1&language=ru&format=json'
-
-	def get_json(self):
-		response = requests.get(self.__url)
-		data = response.json()
-		with open('location.json', 'w', newline='', encoding='utf-8') as file:
-			json.dump(data, file)
-
-	@staticmethod
-	def read_json():
-		with open('location.json', 'r', encoding='utf-8') as file:
-			location_params = json.load(file)
-		city_name = location_params['results'][0]['name']
-		latitude = location_params['results'][0]['latitude']
-		longitude = location_params['results'][0]['longitude']
-		timezone = location_params['results'][0]['timezone']
-		return {'city_name': city_name,
-				'latitude': latitude,
-				'longitude': longitude,
-				'timezone': timezone}
+from requests import Response
 
 
-if __name__ == '__main__':
-	try:
-		location_params = GetLocation.read_json()
-	except:
-		user_city = input('Введите название города: ')
-		user_city_location = GetLocation(user_city)
-		user_city_location.get_json()
-		location_params = user_city_location.read_json()
+class Location:
+    """
+    Класс описывает местоположение.
+    """
 
-	print(location_params['latitude'])
-	print(location_params['longitude'])
-	print(location_params['timezone'].replace('/', '%2F'))
-	print(location_params['city_name'])
+    def __init__(self, city: str):
+        """Устанавливает атрибуты для объекта Location.
+
+        Args:
+            city: Город.
+            url: URL-адрес для получения координат и часового пояса города.
+        """
+        self.__city = city
+        self.__url: str = (
+            f"https://geocoding-api.open-meteo.com/v1/search?name="
+            f"{self.__city}&count=1&language=ru&format=json"
+        )
+
+    def get_json(self):
+        """Запрос данных о городе с сайта, запись их в файл location.json."""
+        response: Response = requests.get(self.__url)
+        data: dict = response.json()
+        with open("location.json", "w", newline="", encoding="utf-8") as file:
+            json.dump(data, file)
+
+    @staticmethod
+    def read_json() -> dict:
+        """Чтение данных о городе из файла location.json.
+
+        Returns:
+            Словарь с названием, координатами и часовым поясом города.
+        """
+        with open("location.json", "r", encoding="utf-8") as file:
+            location_params: dict = json.load(file)
+        city_name: str = location_params["results"][0]["name"]
+        latitude: float = location_params["results"][0]["latitude"]
+        longitude: float = location_params["results"][0]["longitude"]
+        timezone: str = location_params["results"][0]["timezone"]
+        return {
+            "city_name": city_name,
+            "latitude": latitude,
+            "longitude": longitude,
+            "timezone": timezone,
+        }
+
+
+if __name__ == "__main__":
+    user_city = input("Введите название города: ")
+    user_city_location = Location(user_city)
+    user_city_location.get_json()
+    location_params = user_city_location.read_json()
+
+    print(location_params["latitude"])
+    print(location_params["longitude"])
+    print(location_params["timezone"])
+    print(location_params["city_name"])
